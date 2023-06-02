ac06e18e6811a9abfa25f8a985efe4de42c4d6ef
Maarij <maarij.anwar@gmail.com>
2023-06-01 21:47:54 -0500

    dynamodb post

--- space-finder/src/services/spaces/handler.ts
+++ space-finder/src/services/spaces/handler.ts
@@ -1,25 +1,36 @@
 import {APIGatewayProxyEvent, APIGatewayProxyResult, Context} from "aws-lambda";
+import {DynamoDBClient} from "@aws-sdk/client-dynamodb";
+import {postSpaces} from "./PostSpaces";
+
+const ddbClient = new DynamoDBClient({});
 
 async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
 
   let message: string;
 
-  switch (event.httpMethod) {
-    case 'GET':
-      message = 'Hello from GET'
-      break;
-    case 'POST':
-      message = 'Hello from POST'
-      break;
-    default:
-      break;
+  try {
+    switch (event.httpMethod) {
+      case 'GET':
+        message = 'Hello from GET'
+        break;
+      case 'POST':
+        const response = postSpaces(event, ddbClient);
+        return response;
+      default:
+        break;
+    }
+  } catch (e) {
+    console.log(e.message);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e.message)
+    }
   }
 
   const response: APIGatewayProxyResult = {
     statusCode: 200,
     body: JSON.stringify(message)
   }
-  console.log(event);
 
   return response;
 }
